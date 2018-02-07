c017e10a75c2a8cad647c20ad65ab7660a0a8d76
Alexander Burakovskiy <alexander.burakovskiy@gmail.com>
2018-02-07 23:58:06 +0200

    fetch last draw date

--- scrape.php
+++ scrape.php
@@ -5,6 +5,8 @@ require 'vendor/autoload.php';
 use GuzzleHttp\Psr7;
 use GuzzleHttp\Exception\RequestException;
 
+$client = new GuzzleHttp\Client();
+
 $games = [
     0 => [
         'POWERBALL' => [
@@ -65,12 +67,22 @@ $games = [
     ]
 ];
 
-$json = '[
+$jsonLastDate = '[
+   {
+      "query":"{\n  POWERBALLLastDrawDate: lastDrawDate(logicalGameIdentifier: \"POWERBALL\") {\n    date\n    __typename\n  }\n  MEGA_MILLIONSLastDrawDate: lastDrawDate(logicalGameIdentifier: \"MEGA_MILLIONS\") {\n    date\n    __typename\n  }\n  FANTASY_5LastDrawDate: lastDrawDate(logicalGameIdentifier: \"FANTASY_5\") {\n    date\n    __typename\n  }\n  CLASSIC_LOTTO_47LastDrawDate: lastDrawDate(logicalGameIdentifier: \"CLASSIC_LOTTO_47\") {\n    date\n    __typename\n  }\n  DAILY_3LastDrawDate: lastDrawDate(logicalGameIdentifier: \"DAILY_3\") {\n    date\n    __typename\n  }\n  DAILY_4LastDrawDate: lastDrawDate(logicalGameIdentifier: \"DAILY_4\") {\n    date\n    __typename\n  }\n  LUCKY_FOR_LIFELastDrawDate: lastDrawDate(logicalGameIdentifier: \"LUCKY_FOR_LIFE\") {\n    date\n    __typename\n  }\n  POKER_LOTTOLastDrawDate: lastDrawDate(logicalGameIdentifier: \"POKER_LOTTO\") {\n    date\n    __typename\n  }\n  DAILY_KENOLastDrawDate: lastDrawDate(logicalGameIdentifier: \"DAILY_KENO\") {\n    date\n    __typename\n  }\n}\n",
+      "operationName":null
+   }
+]';
+
+$dates = fetch($jsonLastDate);
+$dates = array_shift($dates);
+
+$jsonGames = '[
    {
       "query":"query gameCardData($logicalGameIdentifier: String, $drawDate: String) {\n  jackpot: currentEstimatedJackpotForGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    resultsPending\n    jackpot\n    __typename\n  }\n  nextDrawDate: nextDrawTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  nextBreakDate: nextBreakTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  winningNumbers: winningNumbers(logicalGameIdentifier: $logicalGameIdentifier, drawDate: $drawDate) {\n    resultsPending\n    drawNumbers\n    powerball\n    powerplay\n    __typename\n  }\n  drawingDays: drawingDays(logicalGameIdentifier: $logicalGameIdentifier) {\n    SUNDAY\n    MONDAY\n    TUESDAY\n    WEDNESDAY\n    THURSDAY\n    FRIDAY\n    SATURDAY\n    __typename\n  }\n}\n",
       "variables":{
          "logicalGameIdentifier":"POWERBALL",
-         "drawDate":"2018-02-03T05:00:00.000Z"
+         "drawDate":"POWERBALLLastDrawDate"
       },
       "operationName":"gameCardData"
    },
@@ -78,7 +90,7 @@ $json = '[
       "query":"query megaMillionsData($logicalGameIdentifier: String, $drawDate: String) {\n  jackpot: currentEstimatedJackpotForGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    resultsPending\n    jackpot\n    __typename\n  }\n  nextDrawDate: nextDrawTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  nextBreakDate: nextBreakTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  winningNumbers: winningNumbers(logicalGameIdentifier: $logicalGameIdentifier, drawDate: $drawDate) {\n    resultsPending\n    drawNumbers\n    megaplier\n    megaball\n    __typename\n  }\n  drawingDays: drawingDays(logicalGameIdentifier: $logicalGameIdentifier) {\n    SUNDAY\n    MONDAY\n    TUESDAY\n    WEDNESDAY\n    THURSDAY\n    FRIDAY\n    SATURDAY\n    __typename\n  }\n}\n",
       "variables":{
          "logicalGameIdentifier":"MEGA_MILLIONS",
-         "drawDate":"2018-02-06T05:00:00.000Z"
+         "drawDate":"MEGA_MILLIONSLastDrawDate"
       },
       "operationName":"megaMillionsData"
    },
@@ -86,7 +98,7 @@ $json = '[
       "query":"query fantasy5Data($logicalGameIdentifier: String, $drawDate: String) {\n  jackpot: currentEstimatedJackpotForGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    resultsPending\n    jackpot\n    __typename\n  }\n  nextDrawDate: nextDrawTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  nextBreakDate: nextBreakTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  winningNumbers: winningNumbers(logicalGameIdentifier: $logicalGameIdentifier, drawDate: $drawDate) {\n    resultsPending\n    drawNumbers\n    __typename\n  }\n  drawingDays: drawingDays(logicalGameIdentifier: $logicalGameIdentifier) {\n    SUNDAY\n    MONDAY\n    TUESDAY\n    WEDNESDAY\n    THURSDAY\n    FRIDAY\n    SATURDAY\n    __typename\n  }\n}\n",
       "variables":{
          "logicalGameIdentifier":"FANTASY_5",
-         "drawDate":"2018-02-06T05:00:00.000Z"
+         "drawDate":"FANTASY_5LastDrawDate"
       },
       "operationName":"fantasy5Data"
    },
@@ -94,7 +106,7 @@ $json = '[
       "query":"query lotto47Data($logicalGameIdentifier: String, $drawDate: String) {\n  jackpot: currentEstimatedJackpotForGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    resultsPending\n    jackpot\n    __typename\n  }\n  nextDrawDate: nextDrawTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  nextBreakDate: nextBreakTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  winningNumbers: winningNumbers(logicalGameIdentifier: $logicalGameIdentifier, drawDate: $drawDate) {\n    resultsPending\n    drawNumbers\n    __typename\n  }\n  drawingDays: drawingDays(logicalGameIdentifier: $logicalGameIdentifier) {\n    SUNDAY\n    MONDAY\n    TUESDAY\n    WEDNESDAY\n    THURSDAY\n    FRIDAY\n    SATURDAY\n    __typename\n  }\n}\n",
       "variables":{
          "logicalGameIdentifier":"CLASSIC_LOTTO_47",
-         "drawDate":"2018-02-03T05:00:00.000Z"
+         "drawDate":"CLASSIC_LOTTO_47LastDrawDate"
       },
       "operationName":"lotto47Data"
    },
@@ -106,7 +118,7 @@ $json = '[
       "query":"query daily3Data($logicalGameIdentifier: String, $drawDate: String) {\n  jackpot: currentEstimatedJackpotForGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    jackpot\n    __typename\n  }\n  nextDrawDate: nextDrawTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  nextBreakDate: nextBreakTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  winningNumbers: winningNumbers(logicalGameIdentifier: $logicalGameIdentifier, drawDate: $drawDate) {\n    resultsPending\n    drawNumbersMid\n    drawNumbersEve\n    __typename\n  }\n  drawingDays: drawingDays(logicalGameIdentifier: $logicalGameIdentifier) {\n    SUNDAY\n    MONDAY\n    TUESDAY\n    WEDNESDAY\n    THURSDAY\n    FRIDAY\n    SATURDAY\n    __typename\n  }\n}\n",
       "variables":{
          "logicalGameIdentifier":"DAILY_3",
-         "drawDate":"2018-02-06T05:00:00.000Z"
+         "drawDate":"DAILY_3LastDrawDate"
       },
       "operationName":"daily3Data"
    },
@@ -114,7 +126,7 @@ $json = '[
       "query":"query daily4Data($logicalGameIdentifier: String, $drawDate: String) {\n  jackpot: currentEstimatedJackpotForGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    jackpot\n    __typename\n  }\n  nextDrawDate: nextDrawTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  nextBreakDate: nextBreakTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  winningNumbers: winningNumbers(logicalGameIdentifier: $logicalGameIdentifier, drawDate: $drawDate) {\n    resultsPending\n    drawNumbersMid\n    drawNumbersEve\n    __typename\n  }\n  drawingDays: drawingDays(logicalGameIdentifier: $logicalGameIdentifier) {\n    SUNDAY\n    MONDAY\n    TUESDAY\n    WEDNESDAY\n    THURSDAY\n    FRIDAY\n    SATURDAY\n    __typename\n  }\n}\n",
       "variables":{
          "logicalGameIdentifier":"DAILY_4",
-         "drawDate":"2018-02-06T05:00:00.000Z"
+         "drawDate":"DAILY_4LastDrawDate"
       },
       "operationName":"daily4Data"
    },
@@ -122,7 +134,7 @@ $json = '[
       "query":"query luckyForLifeData($logicalGameIdentifier: String, $drawDate: String) {\n  jackpot: currentEstimatedJackpotForGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    jackpot\n    __typename\n  }\n  nextDrawDate: nextDrawTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  nextBreakDate: nextBreakTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  winningNumbers: winningNumbers(logicalGameIdentifier: $logicalGameIdentifier, drawDate: $drawDate) {\n    resultsPending\n    drawNumbers\n    luckyball\n    __typename\n  }\n  drawingDays: drawingDays(logicalGameIdentifier: $logicalGameIdentifier) {\n    SUNDAY\n    MONDAY\n    TUESDAY\n    WEDNESDAY\n    THURSDAY\n    FRIDAY\n    SATURDAY\n    __typename\n  }\n}\n",
       "variables":{
          "logicalGameIdentifier":"LUCKY_FOR_LIFE",
-         "drawDate":"2018-02-05T05:00:00.000Z"
+         "drawDate":"LUCKY_FOR_LIFELastDrawDate"
       },
       "operationName":"luckyForLifeData"
    },
@@ -130,7 +142,7 @@ $json = '[
       "query":"query pokerLottoData($logicalGameIdentifier: String, $drawDate: String) {\n  jackpot: currentEstimatedJackpotForGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    jackpot\n    __typename\n  }\n  nextDrawDate: nextDrawTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  nextBreakDate: nextBreakTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  winningNumbers: winningNumbers(logicalGameIdentifier: $logicalGameIdentifier, drawDate: $drawDate) {\n    resultsPending\n    drawNumbers\n    __typename\n  }\n  drawingDays: drawingDays(logicalGameIdentifier: $logicalGameIdentifier) {\n    SUNDAY\n    MONDAY\n    TUESDAY\n    WEDNESDAY\n    THURSDAY\n    FRIDAY\n    SATURDAY\n    __typename\n  }\n}\n",
       "variables":{
          "logicalGameIdentifier":"POKER_LOTTO",
-         "drawDate":"2018-02-06T05:00:00.000Z"
+         "drawDate":"POKER_LOTTOLastDrawDate"
       },
       "operationName":"pokerLottoData"
    },
@@ -138,27 +150,17 @@ $json = '[
       "query":"query dailyKenoData($logicalGameIdentifier: String, $drawDate: String) {\n  jackpot: currentEstimatedJackpotForGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    jackpot\n    __typename\n  }\n  nextDrawDate: nextDrawTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  nextBreakDate: nextBreakTimeForLogicalGame(logicalGameIdentifier: $logicalGameIdentifier) {\n    date\n    __typename\n  }\n  winningNumbers: winningNumbers(logicalGameIdentifier: $logicalGameIdentifier, drawDate: $drawDate) {\n    resultsPending\n    drawNumbers\n    __typename\n  }\n  drawingDays: drawingDays(logicalGameIdentifier: $logicalGameIdentifier) {\n    SUNDAY\n    MONDAY\n    TUESDAY\n    WEDNESDAY\n    THURSDAY\n    FRIDAY\n    SATURDAY\n    __typename\n  }\n}\n",
       "variables":{
          "logicalGameIdentifier":"DAILY_KENO",
-         "drawDate":"2018-02-06T05:00:00.000Z"
+         "drawDate":"DAILY_KENOLastDrawDate"
       },
       "operationName":"dailyKenoData"
    }
 ]';
 
-$client = new GuzzleHttp\Client();
-try {
-    $result = $client->request('POST', 'https://www.michiganlottery.com/api', [
-        'json' => json_decode($json)
-    ]);
-} catch (RequestException $e) {
-    echo Psr7\str($e->getRequest());
-    if ($e->hasResponse()) {
-        echo Psr7\str($e->getResponse());
-    }
-    die;
+foreach ($dates['data'] as $key => $date) {
+    $jsonGames = str_replace($key, $date['date'], $jsonGames);
 }
 
-$response = $result->getBody()->getContents();
-$response = json_decode($response, true);
+$response = fetch($jsonGames);
 
 $numbers = [];
 foreach ($response as $key => $item) {
@@ -171,3 +173,23 @@ foreach ($response as $key => $item) {
 
 header('Content-Type: application/json');
 echo json_encode($numbers);
+
+function fetch(string $json): array
+{
+    global $client;
+    try {
+        $result = $client->request('POST', 'https://www.michiganlottery.com/api', [
+            'json' => json_decode($json)
+        ]);
+    } catch (RequestException $e) {
+        echo Psr7\str($e->getRequest());
+        if ($e->hasResponse()) {
+            echo Psr7\str($e->getResponse());
+        }
+        die;
+    }
+
+    $response = $result->getBody()->getContents();
+    $response = json_decode($response, true);
+    return $response;
+}
